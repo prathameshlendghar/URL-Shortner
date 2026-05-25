e5339c45aed706f50b2853bb89dd3c335be9261a
prathamesh <prathameshlendghar78@gmail.com>
2026-05-25 16:13:41 +0530

    Reinitiated the implimentation of project

--- models/response.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type ShortUrlResp struct {
-	LongUrl   string `json:"longurl"`
-	ShortUrl  string `json:"shorturl"`
-	CreatedAt string `json:"createdat,omitempty"`
-	ExpiresAt string `json:"expires,omitempty"`
-	Tag       string `json:"tag,omitempty"`
-}
